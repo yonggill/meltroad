3b7b2c6f5f2fba19bdd311d22b21b76983d1c61f
yongyong <yonggill@wishket.com>
2024-08-11 23:28:09 +0900

    이미지 저장 경로 변경

--- backend/meltroad_backend/image/models.py
+++ backend/meltroad_backend/image/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from rest_framework import serializers
 
-# Create your models here.
+def upload_post_image(instance, filename):
+    return f"post/{instance.post.id}/content-img-{filename}"
+
 class PostImage(models.Model):
     post = models.ForeignKey('post.Post', related_name="images", on_delete=models.CASCADE)
-    image_file = models.ImageField()
+    image_file = models.ImageField(upload_to=upload_post_image)
 
 
 class PostImageSerializer(serializers.ModelSerializer):
--- backend/meltroad_backend/post/models.py
+++ backend/meltroad_backend/post/models.py
@@ -3,12 +3,16 @@ from rest_framework import serializers
 from meltroad_backend.image.models import PostImageSerializer
 # Create your models here.
 
+
+def upload_post_meta_image(instance, filename):
+    return f"post/{instance.post.id}/meta-img-{filename}"
+
 class Post(models.Model):
     title = models.CharField(max_length=100, default="")
     slug = models.CharField(max_length=100, default="")
     content = models.TextField()
     meta_description = models.CharField(max_length=200)
-    meta_image = models.ImageField()
+    meta_image = models.ImageField(upload_to=upload_post_meta_image)
     reading_minutes = models.IntegerField(default=0)
     date_published = models.DateTimeField()
 
